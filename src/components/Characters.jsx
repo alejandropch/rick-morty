9aa4d3f2a12c66f3273aadc65558e142867f4f65
alejandropch <alejandropachas1@gmail.com>
2021-07-10 20:21:20 -0500

    background-color transition modified

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,4 +1,4 @@
-import React ,{ useState, useEffect} from 'react'
+import React ,{ useState, useEffect,useReducer} from 'react'
 
 import '../styles/characters.css'
 
@@ -6,7 +6,31 @@ import '../styles/characters.css'
 export default function Characters() {
     
     
+
 const [character,setCharacter]=useState([])
+
+const initialState =[]
+
+
+
+const reducerCreated=(state,action) => {
+
+    switch(action.type) {
+    
+        case 'FAVORITE_CHARACTER':
+
+            return {
+                ...state,allFavorites:
+                            [...state.favorite, action.payLoad]
+            }
+        
+            default: 
+                return state
+    }
+
+}
+
+const [favorite,dispatch]=useReducer(reducerCreated,initialState)
 
 
 useEffect(()=>{
@@ -18,17 +42,26 @@ fetch('https://rickandmortyapi.com/api/character')
 //when there is not nothing to listen, it will render once
 },[])
 
+
+function handleClick(favorite){
+    dispatch({type:'FAVORITE_CHARACTER',payLoad:favorite})
+    console.log({favorite})
+}
+
+
+
+
     return ( 
     
     <div className="container">
       
         {character.map(character=>(
             
-            <div className="container__tarjeta" key={character.id}>
+            <div onClick={()=>handleClick(character)}className="container__tarjeta" key={character.id}>
         
                 <h2 className="container__tarjeta--name">{character.name}</h2>
         
-                <img className="container__tarjeta--img"src={character.image}/> 
+                <img className="container__tarjeta--img"src={ character.image}/> 
             </div>
              
             ))
