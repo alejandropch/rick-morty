ba725ea8e168038a2ad1bf16bce7da9e0de99692
alejandropch <alejandropachas1@gmail.com>
2021-07-12 09:37:56 -0500

    input created, using state and handleChange events

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -5,67 +5,82 @@ import '../styles/characters.css'
 
 export default function Characters() {
     
-    
-
-const initialState ={
-
-    allFavorites: []
-}
+    const [character,setCharacter]=useState([])
+    const [search, setSearch]=useState('')
 
 
-const reducerCreated=(state,action) => {
+    const initialState ={
 
-    switch(action.type) {
-    
-        case 'FAVORITE_CHARACTER':
-
-            return {
-                ...state,
-                    // the first value is going to add all the 
-                    // characters that you already have, and the second 
-                    // one will add a new one
-                    allFavorites: [...state.allFavorites, action.payLoad]
-            }
-        
-            default: 
-                return state
+        allFavorites: []
     }
 
-}
+
+    const reducerCreated=(state,action) => {
+
+        switch(action.type) {
+        
+            case 'FAVORITE_CHARACTER':
+
+                return {
+                    ...state,
+                        // the first value is going to add all the 
+                        // characters that you already have, and the second 
+                        // one will add a new one
+                        allFavorites: [...state.allFavorites, action.payLoad]
+                }
+            
+                default: 
+                    return state
+        }
+
+    }
 
  
-const [character,setCharacter]=useState([])
 
 
-
-const [favorite,dispatch]=useReducer(reducerCreated,initialState)
-
-
-useEffect(()=>{
-
-fetch('https://rickandmortyapi.com/api/character')
-.then(responce=>responce.json())
-    .then(data=>setCharacter(data.results))
-
-//when there is not nothing to listen, it will render once
-},[])
+    const [favorite,dispatch]=useReducer(reducerCreated,initialState)
 
 
-function handleClick(favorite){
-    console.log(favorite)
-    dispatch({type:'FAVORITE_CHARACTER',payLoad:favorite})
-}
+    useEffect(()=>{
+
+    fetch('https://rickandmortyapi.com/api/character')
+    .then(responce=>responce.json())
+        .then(data=>setCharacter(data.results))
+
+    //when there is not nothing to listen, it will render once
+    },[])
 
 
+    function handleClick(favorite){
+        dispatch({type:'FAVORITE_CHARACTER',payLoad:favorite})
+    }
+
+
+    const handleChange=(e)=>{
+
+        setSearch(e.target.value)
+        console.log(e)
+
+    }
+
+    
 
 
     return ( 
         <React.Fragment>
           
     <div className="container">
-        <div className="favorite">
+
+        <div className="container__search">
+            <input type="text" onChange={handleChange} value={search}></input>
+
+        </div>
+
+
+
+        <div className="container__favorite">
                 
-            {favorite.allFavorites.map(favoriteCharacter=><img key={favoriteCharacter.id} className="favorite__characters" src={favoriteCharacter.image} />)
+            {favorite.allFavorites.map(favoriteCharacter=><img key={favoriteCharacter.id} className="container__favorite--characters" src={favoriteCharacter.image} />)
             }
 
         </div>
